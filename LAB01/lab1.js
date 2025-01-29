fe9fba281bfd512514ffe72fe9056fb97d19cef2
Sebas4120 <sebastianaguirrem49@gmail.com>
2025-01-28 19:48:41 -0500

    Fixing conditionals and user input

--- LAB01/lab1.js
+++ LAB01/lab1.js
@@ -1,7 +1,3 @@
-//Hello World Console App with Node.js
-//Most syntax is the same as JavaScript for the browser
-//console.log('Hello world!');
-
 //Import the modules
 
 var prompt = require('prompt');
@@ -11,68 +7,70 @@ prompt.start();
 //Ask user to enter an input
 
 prompt.get([{
-  name: 'userChoice',
-  description: '(1) Rock / (2) Paper / (3) Scissors',
+  name: 'choice',
+  description: 'Welcome to my game! please, choose an option: (1) Rock / (2) Paper / (3) Scissors',
   required: true,
-  integer: true,
   conform: function (value){
-    return value === 1 || value === 2 || value === 3;
+    const num = Number(value)
+    return num === 1 || num === 2 || num === 3;
   }
 }], function (err, result) {
 
+  if(err){
+    console.error('Error:',err);
+    return;
+  }
+
+  //Mapping user selection
+
+  const choices ={
+    '1' : 'Rock',
+    '2' : 'Paper',
+    '3' : 'Scissors'
+  }
+
+  //Convert user choice to a string 
+  const userChoice = choices[result.choice]
+  
   // Log the results of the user.
 
-console.log('User selection: ');
-
-let userChoice = '';
-switch (result.userChoice){
-  case 1:
-    userChoice = 'Rock';
-    break;
-  
-  case 2:
-    userChoice = 'Paper';
-    break;
-
-  case 3:
-    userChoice = 'Scissors';
-    break;  
-}
-
-// Generate the computer's answer (using random function between 0 and 1)
-const randomNumber = Math.random();
-let computerChoice = '';
-
-if (randomNumber <= 0.34){
-  computerChoice = 'Rock';
-}else if (randomNumber <= 0.67){
-  computerChoice = 'Scissors';
-}else{
-  computerChoice = 'Paper';
-}
-
-console.log('Computer selection:' + computerChoice);
+  console.log('User selection: ' + userChoice);
 
 
 
+  // Generate the computer's answer (using random function between 0 and 1)
+  const randomNumber = Math.random();
+  let computerChoice;
+
+  if (randomNumber <= 0.34){
+    computerChoice = 'Rock';
+  }else if (randomNumber <= 0.67){
+    computerChoice = 'Scissors';
+  }else{
+    computerChoice = 'Paper';
+  }
+
+  console.log('Computer selection:' + computerChoice);
 
 
+  // Determine the winner using conditionals (if-else)
 
+  let resultMessage = '';
 
+  if(userChoice === computerChoice){
+    resultMessage = "It is a tie!"
+  }else if(
+  (userChoice === 'Rock' && computerChoice === 'Scissors')||
+  (userChoice === 'Scissors' && computerChoice === 'Paper') ||
+  (userChoice === 'Paper' && computerChoice === 'Rock')
+  ){
+    resultMessage = "User wins!!"
+  }else{
+    resultMessage = "Computer wins!!"
+  }
 
-
-
-
-
-
+  console.log(resultMessage);
 
 
 });
 
-//Generate computer answer by Math.random() function to generate a number as computerSelection
-
-
-//Make a conditional to compare both response and get a winner
-
-
-//Display the winner/loser/tie
