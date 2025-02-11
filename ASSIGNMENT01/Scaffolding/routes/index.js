130bd387751b7cece8fb58d895ccc2938854329c
Sebas4120 <sebastianaguirrem49@gmail.com>
2025-02-11 17:10:28 -0500

    Building layout and stylesheets

--- ASSIGNMENT01/Scaffolding/routes/index.js
+++ ASSIGNMENT01/Scaffolding/routes/index.js
@@ -3,19 +3,20 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'My Portafolio' });
+  res.render('index', { title: 'My Portfolio', headerTitle: 'Hey, I am Sebastian Aguirre',
+    headerDescription: 'Discover my work and projects.'});
 });
 
 router.get('/about', function(req, res, next) {
-  res.render('about', { title: 'About' });
+  res.render('about', { title: 'About', bodyClass: 'about' });
 });
 
 router.get('/projects', function(req, res, next) {
-  res.render('projects', { title: 'Projects' });
+  res.render('projects', { title: 'Projects', bodyClass: 'projects' });
 });
 
 router.get('/contact', function(req, res, next) {
-  res.render('contact', { title: 'Contact' });
+  res.render('contact', { title: 'Contact', bodyClass: 'contact' });
 });
 
 module.exports = router;
