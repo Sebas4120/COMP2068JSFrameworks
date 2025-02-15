05f5031b64c0186ddfd5dfdac1b91a20a96a1491
Sebas4120 <sebastianaguirrem49@gmail.com>
2025-02-15 17:07:40 -0500

    Building my about mw section and detailing home page
    I add my background and my current studies. Also, I fixed some details in my home page

--- ASSIGNMENT01/Scaffolding/routes/index.js
+++ ASSIGNMENT01/Scaffolding/routes/index.js
@@ -3,20 +3,23 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'My Portfolio', headerTitle: 'Hey, I am Sebastian Aguirre',
+  res.render('index', { title: 'My Portfolio', headerTitle: 'Hey!, I\'m Sebastian Aguirre',
     headerDescription: 'Discover my work and projects.'});
 });
 
 router.get('/about', function(req, res, next) {
-  res.render('about', { title: 'About', bodyClass: 'about' });
+  res.render('about', { title: 'About', headerTitle: 'About Me',
+    headerDescription: 'Learn more about my background and experience.' });
 });
 
 router.get('/projects', function(req, res, next) {
-  res.render('projects', { title: 'Projects', bodyClass: 'projects' });
+  res.render('projects', { title: 'Projects', headerTitle: 'My projects',
+    headerDescription: 'Here are some of the projects I have worked on' });
 });
 
 router.get('/contact', function(req, res, next) {
-  res.render('contact', { title: 'Contact', bodyClass: 'contact' });
+  res.render('contact', { title: 'Contact', headerTitle: 'Contact Me',
+    headerDescription: 'Get in touch with me.' });
 });
 
 module.exports = router;
