3f91625b9b7d08cc372c7c5a2e9511fd5bb5b9e4
Sebas4120 <sebastianaguirrem49@gmail.com>
2025-03-12 20:26:15 -0400

    week 7

--- Lessons/Lesson05/configs/globals.js
+++ Lessons/Lesson05/configs/globals.js
@@ -1,11 +1,16 @@
-//This is my application-level configuration object
-//Require and initialize dotenv
 require("dotenv").config();
-//Create a configuration object
+// Global configurations object contains Application Level variables such as:
+// client secrets, passwords, connection strings, and misc flags
 const configurations = {
-  ConnectionStrings:{
-    MongoDB: process.env.CONNECTION_STRING_MONGODB
-  }
-}
-//Export the configuration object
+  ConnectionStrings: {
+    MongoDB: process.env.CONNECTION_STRING_MONGODB,
+  },
+  Authentication: {
+    GitHub: {
+      ClientId: process.env.GITHUB_CLIENT_ID,
+      ClientSecret: process.env.GITHUB_CLIENT_SECRET,
+      CallbackUrl: process.env.GITHUB_CALLBACK_URL
+    },
+  },
+};
 module.exports = configurations;
